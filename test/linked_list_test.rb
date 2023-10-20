aa2cb3953360e3c3553eeb3fe765d794143ee122
Dan Briechle <danbriechle@hotmail.com>
2023-10-20 16:48:56 -0600

    Fix: correct test filename

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require_relative '../app/linked-list'
+require_relative '../app/linked_list.rb'
 
 class LinkedListTest < Minitest::Test
 end
