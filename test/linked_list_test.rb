8429676ccb0d6ceb2c8b38eb77be68fc41422a85
Dan Briechle <danbriechle@hotmail.com>
2023-10-20 17:04:16 -0600

    Chore: Add commented out test cases to test file

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -4,4 +4,12 @@ require 'minitest/autorun'
 require_relative '../app/linked_list.rb'
 
 class LinkedListTest < Minitest::Test
+  #is_empty?: return true if the linked list is empty
+  #push: given a data, add a new node in the end
+  #unshift: add a new node in the front
+  #pop: remove the last node and return it
+  #shift: remove the first node and return it
+  #size: return the length of linked list
+  #pretty_print: print the current linked list as an array
+  #clear: clear the whole linked list  
 end
