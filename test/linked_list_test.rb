ae0ba5fef0da000a1575d55dd5b60a55d87d6596
Dan Briechle <danbriechle@hotmail.com>
2023-10-21 15:31:12 -0600

    Feat: Add tests for list size and addition

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -9,12 +9,25 @@ class LinkedListTest < Minitest::Test
     assert_instance_of(LinkedList, LinkedList.new('string'))
   end
 
-  # is_empty?: return true if the linked list is empty
+  # size: return the length of linked list
+  def test_it_knows_its_length
+    list = LinkedList.new('string')
+
+    assert_equal(list.size(1))
+  end
+  
   # push: given a data, add a new node in the end
+  def test_it_can_be_added_to
+    list = LinkedList.new('string')
+    list.push('new string')
+
+    assert_equal(list.size(2))
+  end
+
+  # is_empty?: return true if the linked list is empty
   # unshift: add a new node in the front
   # pop: remove the last node and return it
   # shift: remove the first node and return it
-  # size: return the length of linked list
   # pretty_print: print the current linked list as an array
   # clear: clear the whole linked list
 end
